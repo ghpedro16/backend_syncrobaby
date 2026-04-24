43fd23d8a92e4bc74c09e2a0986d7181dbfa1755
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 11:09:52 -0300

    estrutura inicial dos arquivos model e controller referente ao crud de produto

---
+++ model/product.js
@@ -0,0 +1,30 @@
+/******************************************************************************************************
+ * Objetivo: Arquivo responsavel pelo CRUD referente a tabela de PRODUTO da aplicação SyncroBaby
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************/
+
+//Criar variavel para conexao com o banco
+
+const getProductByType = async function (product_type) {
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if (Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+const setInsertProduct = async function () {
+
+}
