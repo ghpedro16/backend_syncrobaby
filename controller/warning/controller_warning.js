90dec692ca99dedbabfa364c62307f993ead43e5
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 10:09:57 -0300

    estrutura inicial dos arquivos model e controller referente ao crud de notificacoes

--- controller/warning/controller_warning.js
+++ controller/warning/controller_warning.js
@@ -1,26 +1,22 @@
 /******************************************************************************************************************************************
  * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
- * Data: 22/04/2026
+ * Data: 24/04/2026
  * Autor: SyncroBaby
  * Versão: 1.0
  * ****************************************************************************************************************************************/
 
-const childrenDAO = require('../../model/user.js')
+const warningDAO = require('../../model/warning.js')
 
 const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
 
-const listUser = async function (id) {
+const listWarningByUser = async function (id_user) {
 
 }
 
-const insertUser = async function () {
+const insertWarningByChildren = async function (id_children) {
 
 }
 
-const updateUser = async function (id) {
-
-}
-
-const deleteUser = async function (id) {
+const deleteWarning = async function (id) {
 
 }
--- model/warning.js
+++ model/warning.js
@@ -1,13 +1,13 @@
 /******************************************************************************************************
- * Objetivo: Arquivo responsavel pelo CRUD referente a tabela de USUÁRIO da aplicação SyncroBaby
- * Data: 22/04/2026
+ * Objetivo: Arquivo responsavel pelo CRUD referente a tabela de NOTIFICACAO da aplicação SyncroBaby
+ * Data: 24/04/2026
  * Autor: SyncroBaby
  * Versão: 1.0
  * ****************************************************************************************************/
 
 //Criar variavel para conexao com o banco
 
-const getUserById = async function (id) {
+const getWarningByUserId = async function (id_user) {
     try {
         //Script sql
         let sql = null
@@ -25,12 +25,22 @@ const getUserById = async function (id) {
     }
 }
 
-const setInsertUser = async function () {
+const getWarningByChildrenId = async function (id_children) {
+    try {
+        //Script sql
+        let sql = null
 
-}
+        //Variavel de encaminhamento ao banco
+        let result = null
 
-const setUpdateUser = async function (id) {
+        if (Array.isArray(result))
+            return result
+        else
+            return false
 
+    } catch (error) {
+        return false
+    }
 }
 
 const setDeleteUser = async function (id) {
