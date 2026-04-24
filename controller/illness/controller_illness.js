3af2564b8e2326cfb7970d529ef3283f85c4a27d
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 09:43:09 -0300

    estrutura inicial dos arquivos model e controller referente ao crud de enfermidade

---
+++ controller/illness/controller_illness.js
@@ -0,0 +1,26 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************************************************/
+
+const illnessDAO = require('../../model/illness.js')
+
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+
+const listIllnessByChildrenId = async function (id_children) {
+
+}
+
+const listIllnessByType = async function (id_type) {
+
+}
+
+const insertIllness = async function () {
+
+}
+
+const deleteIllness = async function (id) {
+
+}
