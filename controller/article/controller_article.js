59ca6f480e1be01a15c5b6318d9dc8866f8a04ee
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 10:30:05 -0300

    estrutura inicial dos arquivos model e controller referente ao crud de artigos

---
+++ controller/article/controller_article.js
@@ -0,0 +1,18 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************************************************/
+
+const articleDAO = require('../../model/article.js')
+
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+
+const listAllArticles = async function () {
+
+}
+
+const listArticleByAgeGroup = async function (age_group) {
+
+}
