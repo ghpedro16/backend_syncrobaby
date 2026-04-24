420dd4d4928f06b4aefe715cf127c876e2fbd575
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 08:59:38 -0300

    estrutura inicial dos arquivos model e controller referente ao crud de todas as tabelas de registro de rotinas

---
+++ controller/routines/controller_routines.js
@@ -0,0 +1,14 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+*****************************************************************************************************************************************/
+
+const routinesDAO = require('../../model/routines.js')
+
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+ 
+const listRoutines = async function(id_children, date){
+
+}
---
+++ controller/routines/shower/controller_shower.js
@@ -0,0 +1,18 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+*****************************************************************************************************************************************/
+
+const showerDAO = require('../../../model/shower.js')
+
+const DEFAULT_MESSAGES = require('../../modulo/config_messages.js')
+ 
+const insertShower = async function(){
+ 
+}
+ 
+const deleteShower = async function(id){
+ 
+}
