eefd1686b6a213cf31d7ae2838b74b22a4042b22
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-24 08:10:58 -0300

    estrutura inicial dos arquivos model e controller do crud de vacinas

---
+++ controller/vaccine/controller_vaccine.js
@@ -0,0 +1,23 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************************************************/
+
+ const vaccineDAO = require('../../model/vaccine.js')
+
+ const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+ 
+ const listVaccineByStatus = async function(id_status){
+ 
+ }
+ 
+ const listVaccines = async function(){
+ 
+ }
+ 
+ const updateVaccine = async function(id){
+ 
+ }
+ 
---
+++ model/vaccine.js
@@ -0,0 +1,48 @@
+/******************************************************************************************************
+ * Objetivo: Arquivo responsavel pelo CRUD referente a tabela de VACINA da aplicação SyncroBaby
+ * Data: 24/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************/
+
+//Criar variavel para conexao com o banco
+ 
+const getVaccineByStatus = async function(id_status){
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+   } catch (error) {
+        return false
+   }
+}
+
+const getAllVaccines = async function(){
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+   } catch (error) {
+        return false
+   }
+}
+
+const setUpdateVaccine = async function(id){
+
+}
