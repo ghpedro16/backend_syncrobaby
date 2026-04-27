b0524852223cba768c265488a5b9a1197d7fb633
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-27 11:00:15 -0300

    validações do arquivo controller em desenvolvimento

--- controller/user/controller_user.js
+++ controller/user/controller_user.js
@@ -5,22 +5,187 @@
  * Versão: 1.0
  * ****************************************************************************************************************************************/
 
-const childrenDAO = require('../../model/user.js')
+const userDAO = require('../../model/user.js')
 
 const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
 
-const listUser = async function(id){
+const listUserId = async function(id){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        let resultUser = await userDAO.getUserById(id)
+
+        if(resultUser){
+            if(resultUser.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.response.user = resultUser
+
+                return MESSAGES.DEFAULT_HEADER // 200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
-const insertUser = async function(){
+const listUserLogin = async function(email, password){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        let resultUser = await userDAO.getUserByLogin(email, password)
+
+        if(resultUser){
+            if(resultUser.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.response.user = resultUser
+
+                return MESSAGES.DEFAULT_HEADER // 200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
-const updateUser = async function(id){
+const insertUser = async function(user, contentType){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        if (String(contentType).toUpperCase() == 'APPLICATION/JSON') {
+
+            let validar = await validarDados(user)
+
+            if(!validar){
+
+                let resultUser = userDAO.setInsertUser(user)
+
+                if(resultUser){
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_CREATE_ITEM.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_CREATE_ITEM.status_code
+                    MESSAGES.DEFAULT_HEADER.response = user
+
+                    return MESSAGES.DEFAULT_HEADER // 201
+                }else{
+                    return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+                }
+            }else{
+                return validar
+            }
+        } else {
+            return MESSAGES.ERROR_CONTENT_TYPE // 415
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
+}
+
+const updateUser = async function(user, id, contentType){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    try {
+        if (String(contentType).toUpperCase() == 'APPLICATION/JSON') {
+            
+            let validar = validarDados(user)
+
+            if(!validar){
+                let validarId = await listUserId(id)
+
+                if(validarId.status_code == 200){
+                    user.id = Number(id)
+
+                    let resultUser = await userDAO.setUpdateUser(user)
+
+                    if(resultUser){
+                        MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_UPDATE_ITEM.status
+                        MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_UPDATE_ITEM.status_code
+                        MESSAGES.DEFAULT_HEADER.response.user = user
+
+                        return MESSAGES.DEFAULT_HEADER // 200
+                    }else{
+                        return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+                    }
+                }else{
+                    return validarId
+                }
+            }else{
+                return validar
+            }
+        }else{
+            return MESSAGES.ERROR_CONTENT_TYPE // 415
+        }
+    } catch (error) {
+        MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
 const deleteUser = async function(id){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        if (isNaN(id) && id != '' && id != null && id > 0) {
+            let validarId = await listUserId(id)
+
+            if(validarId.status_code == 200){
+
+                let resultUser = await userDAO.setDeleteUser(id)
+
+                if(resultUser){
+                    MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_DELETE_ITEM.status
+                    MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_DELETE_ITEM.status_code
+                    MESSAGES.DEFAULT_HEADER.message = MESSAGES.SUCCESS_DELETE_ITEM.message
+    
+                    return MESSAGES.DEFAULT_HEADER // 200
+                }else{
+                    return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+                }
+            }else{
+                MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [ID Incorreto!]'
+            return MESSAGES.ERROR_REQUIRED_FIELDS // 400
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
+}
+
+const validarDados = async function(user){
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+
+    if(user.nome == '' || user.nome == undefined || user.nome == null || user.nome.length > 150){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Nome incorreto]'
+        return MESSAGES.ERROR_REQUIRED_FIELDS
+
+    }else if(user.email == '' || user.email == undefined || user.email == null || user.email.length > 255){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [E-mail incorreto]'
+        return MESSAGES.ERROR_REQUIRED_FIELDS
+
+    }else if(user.senha == '' || user.senha == undefined || user.senha == null || user.senha.length > 15){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [Senha incorreto]'
+        return MESSAGES.ERROR_REQUIRED_FIELDS
+
+    }else if(user.foto_perfil == undefined || user.foto_perfil.length > 255){
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [URL da Foto incorreto]'
+    }else{
+        return false
+    }
+}
+
+module.exports = {
+    listUserId,
+    listUserLogin,
+    insertUser,
+    updateUser,
+    deleteUser
 }
--- model/user.js
+++ model/user.js
@@ -6,33 +6,59 @@
  * ****************************************************************************************************/
 
 //Criar variavel para conexao com o banco
- 
- const getUserById = async function(id){
-     try {
-         //Script sql
-         let sql = null
- 
-         //Variavel de encaminhamento ao banco
-         let result = null
- 
-         if(Array.isArray(result))
-             return result
-         else
-             return false
- 
+
+const getUserById = async function (id) {
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if (Array.isArray(result))
+            return result
+        else
+            return false
+
     } catch (error) {
-         return false
+        return false
     }
- }
- 
- const setInsertUser = async function(){
- 
- }
- 
- const setUpdateUser = async function(id){
- 
- }
- 
- const setDeleteUser = async function(id){
- 
- }
+}
+
+const getUserByLogin = async function (email, password) {
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if (Array.isArray(result))
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+const setInsertUser = async function (user) {
+
+}
+
+const setUpdateUser = async function (id, user) {
+
+}
+
+const setDeleteUser = async function (id) {
+
+}
+
+module.exports = {
+    getUserById,
+    getUserByLogin,
+    setInsertUser,
+    setUpdateUser,
+    setDeleteUser
+}
