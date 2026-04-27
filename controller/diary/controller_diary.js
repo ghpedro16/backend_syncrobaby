96e870a34f9f39611c812952f87284bda110aad8
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-04-27 09:41:00 -0300

    estrutura inicial dos arquivos model e controller para crud de anotações do diário

---
+++ controller/diary/controller_diary.js
@@ -0,0 +1,30 @@
+/******************************************************************************************************************************************
+ * Objetivo: Arquivo responsavel pela manipulação dos dados entre a model (banco de dados) e o arquivo app (inicializacão do endpoint)
+ * Data: 27/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************************************************/
+
+const diaryDAO = require('../../model/diary.js')
+
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
+
+const listDiaryByChildren = async function (id) {
+
+}
+
+const listDiaryById = async function (id) {
+
+}
+
+const insertDiary = async function () {
+
+}
+
+const updateDiary = async function (id) {
+
+}
+
+const deleteDiary = async function (id) {
+
+}
---
+++ model/diary.js
@@ -0,0 +1,56 @@
+/******************************************************************************************************
+ * Objetivo: Arquivo responsavel pelo CRUD referente a tabela de DIÁRIO da aplicação SyncroBaby
+ * Data: 27/04/2026
+ * Autor: SyncroBaby
+ * Versão: 1.0
+ * ****************************************************************************************************/
+
+//Criar variavel para conexao com o banco
+ 
+const getDiaryByChildrenId = async function(id_children){
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+   } catch (error) {
+        return false
+   }
+}
+
+const getDiaryById = async function(id){
+    try {
+        //Script sql
+        let sql = null
+
+        //Variavel de encaminhamento ao banco
+        let result = null
+
+        if(Array.isArray(result))
+            return result
+        else
+            return false
+
+   } catch (error) {
+        return false
+   }
+}
+
+const setInsertDiary = async function(){
+
+}
+
+const setUpdateDiary = async function(id){
+
+}
+
+const setDeleteDiary = async function(id){
+
+}
