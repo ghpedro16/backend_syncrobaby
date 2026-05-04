f5d14869c6e3da7eb61f4535b47f5345b48401ca
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-05-04 10:57:09 -0300

    feat: arquivos model e controller do crud de vacinas

--- controller/vaccine/controller_vaccine.js
+++ controller/vaccine/controller_vaccine.js
@@ -10,14 +10,79 @@ const vaccineDAO = require('../../model/vaccine.js')
 const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
 
 const listVaccineByStatus = async function (status) {
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        let resultVaccine = await vaccineDAO.getVaccineByStatus(status)
+
+        if(resultVaccine){
+            if(resultVaccine.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.response.vaccine = resultVaccine
+
+                return MESSAGES.DEFAULT_HEADER // 200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
 const listVaccines = async function () {
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        let resultVaccine = await vaccineDAO.getAllVaccines()
+
+        if(resultVaccine){
+            if(resultVaccine.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.response.vaccine = resultVaccine
+
+                return MESSAGES.DEFAULT_HEADER // 200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
 const listVaccineByAgeGroup = async function (age_group) {
+    let MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
 
+    try {
+        let resultVaccine = await vaccineDAO.getVaccineByAgeGroup(age_group)
+
+        if(resultVaccine){
+            if(resultVaccine.length > 0){
+                MESSAGES.DEFAULT_HEADER.status = MESSAGES.SUCCESS_REQUEST.status
+                MESSAGES.DEFAULT_HEADER.status_code = MESSAGES.SUCCESS_REQUEST.status_code
+                MESSAGES.DEFAULT_HEADER.response.vaccine = resultVaccine
+
+                return MESSAGES.DEFAULT_HEADER // 200
+            }else{
+                return MESSAGES.ERROR_NOT_FOUND // 404
+            }
+        }else{
+            return MESSAGES.ERROR_INTERNAL_SERVER_MODEL // 500
+        }
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER // 500
+    }
 }
 
+module.exports = {
+    listVaccines,
+    listVaccineByStatus,
+    listVaccineByAgeGroup
+}
