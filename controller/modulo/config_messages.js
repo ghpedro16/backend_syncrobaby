cd79f06c026be9487367bcf72208bbed73f16ac4
ghpedro16 <pedrohenrique.phas16@gmail.com>
2026-05-17 14:53:24 -0300

    debug: cabeçalho de resposta

--- controller/modulo/config_messages.js
+++ controller/modulo/config_messages.js
@@ -9,17 +9,7 @@ const dataAtual = new Date()
 
 /********************************************************************   MENSAGENS PADRONIZADAS  ********************************************************************/
 
-const DEFAULT_HEADER = {
-
-    development: 'SyncroBaby',
-    api_description: 'API para manipular dados da aplicação SyncroBaby',
-    status: Boolean,
-    status_code: Number,
-    request_date: dataAtual.toLocaleString(),
-    response: {}
-
-}
-
+const DEFAULT_HEADER = {status_code: Number}
 
 /********************************************************************   MENSAGENS DE SUCESSO    ********************************************************************/
 
@@ -27,9 +17,15 @@ const SUCCESS_REQUEST = {status: true, status_code: 200, message: 'Requisição 
 
 const SUCCESS_CREATE_ITEM = {status: true, status_code: 201, message: 'Requisição bem sucedida...'}
 
-const SUCCESS_UPDATED_ITEM = {status: true, status_code: 200, message: 'Item atualizado com sucesso...'}
+const SUCCESS_UPDATE_ITEM = {status: true, status_code: 200, message: 'Item atualizado com sucesso...'}
 
-const SUCCESS_DELETED_ITEM = {status: true, status_code: 200, message: 'Item excluído com sucesso...'}
+const SUCCESS_DELETE_ITEM = {status: true, status_code: 200, message: 'Item excluído com sucesso...'}
+
+const SUCCESS_MODIFIED_ITEM = {status: true, status_code: 204, message: 'Item modificado com sucesso!'}
+
+const SUCCESS_REACTIVATE_ITEM = {status: true, status_code: 200, message: 'Item reativado com sucesso!'}
+
+const SUCCESS_DEACTIVATE_ITEM = {status: true, status_code: 204, message: 'Item desativado com sucesso!'}
 
 /********************************************************************    MENSAGENS DE ERRO      ********************************************************************/
 
@@ -45,6 +41,12 @@ const ERROR_CONTENT_TYPE = {status: false, status_code: 415, message: 'Não foi 
 
 const ERROR_RELATIONAL_INSERTION = {status: false, status_code: 500, message: 'A requisição do item principal foi processada com sucesso, porém houveram problemas ao inserir dados na tabela de relação!'}
 
+const ERROR_UNIQUE_CONFLICT = {status: false, status_code: 409, message: "Não foi possivel processar a requisição pois o item principal causará um conflito na base de dados."}
+
+const ERROR_DISABLED_USER = {status: false, status_code: 403, message: "Não foi possível acessar esse usuário."}
+
+const ERROR_INVALID_PASSWORD = {status: false, status_code: 401, message: "Senha incorreta!"}
+
 module.exports = {
     DEFAULT_HEADER,
     SUCCESS_REQUEST,
@@ -54,7 +56,13 @@ module.exports = {
     ERROR_REQUIRED_FIELDS,
     SUCCESS_CREATE_ITEM,
     ERROR_CONTENT_TYPE,
-    SUCCESS_UPDATED_ITEM,
-    SUCCESS_DELETED_ITEM,
-    ERROR_RELATIONAL_INSERTION
+    SUCCESS_UPDATE_ITEM,
+    SUCCESS_DELETE_ITEM,
+    ERROR_RELATIONAL_INSERTION,
+    ERROR_UNIQUE_CONFLICT,
+    ERROR_DISABLED_USER,
+    ERROR_INVALID_PASSWORD,
+    SUCCESS_MODIFIED_ITEM,
+    SUCCESS_DEACTIVATE_ITEM,
+    SUCCESS_REACTIVATE_ITEM
 }
